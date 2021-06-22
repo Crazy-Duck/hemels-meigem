3005303a436ce538896ad7c9b5f28bd7e94f468f
Crazy-Duck <jobuyse@gmail.com>
2021-06-22 21:26:39 +0200

    feat: Add reservation links

--- script.js
+++
@@ -1,8 +0,0 @@
-(() => {
-    [].forEach.call(document.getElementsByClassName("offer"), el => {
-        el.addEventListener("click", () => {
-            el.querySelector(".front").classList.toggle("hide");
-            el.querySelector(".back").classList.toggle("hide");
-        });
-    });
-})();
